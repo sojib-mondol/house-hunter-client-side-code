c276aa432b0185141459e3a22a5b0a251e4694fa
sojib-sudo <mdsojibmandol@gmail.com>
2023-07-18 13:23:15 +0600

    login and signup page design

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/main/Main";
 import Home from "../pages/home/Home";
 import PageNotFound from "../pages/shared/pageNotFound/PageNotFound";
+import SignUp from "../pages/shared/signUp/SignUp";
+import Login from "../pages/shared/login/Login";
 
 const router = createBrowserRouter([
     {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element: <Home/>
+            },
+            {
+                path:'/signup',
+                element: <SignUp/>
+            },
+            {
+                path:'/login',
+                element: <Login/>
             },
 
         ]
